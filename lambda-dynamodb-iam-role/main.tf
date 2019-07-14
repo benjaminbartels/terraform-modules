35fd48385d9305b41f526ba9a5cfd91a4063587a
benjaminbartels <bbartels@gmail.com>
2019-07-14 14:26:44 -0600

    fix policies

--- lambda-dynamodb-iam-role/main.tf
+++ lambda-dynamodb-iam-role/main.tf
@@ -7,38 +7,43 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
-data "aws_iam_policy_document" "executor_policy" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-data "aws_iam_policy_document" "crud_policy" {
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    actions = [
-      "dynamodb:Scan",
-      "dynamodb:GetItem",
-      "dynamodb:PutItem",
-      "dynamodb:DeleteItem"
-    ]
-    resources = ["*"]
-  }
-}
 
 resource "aws_iam_role" "lambda_executor" {
   name               = "lambda_${var.app_name}_executor"
-  assume_role_policy = "${data.aws_iam_policy_document.executor_policy}"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com""
+        },
+        "Effect": "Allow",
+      }
+    ]
+  }
+EOF
 }
 
 resource "aws_iam_role_policy" "crud_role" {
-  name   = "dynamodb-item-crud-role"
-  role   = "${aws_iam_role.lambda_executor.id}"
-  policy = "${data.aws_iam_policy_document.crud_policy}"
+  name = "dynamodb-item-crud-role"
+  role = "${aws_iam_role.lambda_executor.id}"
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "dynamodb:Scan",
+              "dynamodb:GetItem",
+              "dynamodb:PutItem",
+              "dynamodb:DeleteItem"
+            ],
+            "Resource": "*"
+        }
+    ]
+  }
+EOF
 }
